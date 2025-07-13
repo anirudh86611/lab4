bdfd84cf45051451d7a647ed2faf4a2c7d5e5ca2
anirudh86611 <2021wa86611@wilp.bits-pilani.ac.in>
2025-07-13 21:37:30 +0530

    initial comit

---
+++ src/main/java/com/example/F1Servlet.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class F1Servlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><head><title>Formula 1 Info</title></head><body>");
+        out.println("<h1>Welcome to Formula 1 Stats</h1>");
+        out.println("<ul>");
+        out.println("<li>2024 Champion: Max Verstappen</li>");
+        out.println("<li>Top Team: Red Bull Racing</li>");
+        out.println("<li>Upcoming Race: Italian Grand Prix - Monza</li>");
+        out.println("</ul>");
+        out.println("</body></html>");
+    }
+}
+
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Formula 1 Dashboard - Lab4</title>
+</head>
+<body>
+<h1>Welcome to Lab4 - Formula 1 Dashboard</h1>
+<p><a href="f1">Click here to view Formula 1 stats</a></p>
+</body>
+</html>
